d385f88fa529da4c7d4aeb2e535d0f832484675a
RaxorLight <huyvu.ng0112@gmail.com>
2025-09-01 21:20:49 -0700

    started new code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class Test extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor frontRight = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backLeft = hardwareMap.dcMotor.get("backLeft");
+        DcMotor backRight = hardwareMap.dcMotor.get("backRight");
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        
+        waitForStart();
+
+        while (opModeIsActive()){
+
+            double multiplier = 0.5;
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) * multiplier / denominator;
+            double backLeftPower = (y - x + rx) * multiplier / denominator;
+            double frontRightPower = (y - x - rx) * multiplier / denominator;
+            double backRightPower = (y + x - rx) * multiplier / denominator;
+
+
+            frontLeft.setPower(frontLeftPower);
+            frontRight.setPower(frontRightPower);
+            backLeft.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
+
+
+            telemetry.addData("y", y);
+            telemetry.addData("x", x);
+            telemetry.addData("rx", rx);
+            telemetry.addData("frontLeftPower", frontLeftPower);
+            telemetry.addData("frontRightPower", frontRightPower);
+            telemetry.addData("backLeftPower", backLeftPower);
+            telemetry.addData("backRightPower", backRightPower);
+            telemetry.update();
+
+        }
+    }
+}
