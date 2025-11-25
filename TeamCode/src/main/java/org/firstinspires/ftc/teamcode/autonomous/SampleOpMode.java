d405dbbfb4e1afd43d1225a8a5357d883a3d5f35
elsadang2021-ux <elsadang2021@gmail.com>
2025-11-25 15:44:48 -0800

    New movement function that decreases time but keeps accuracy

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SampleOpMode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import org.firstinspires.ftc.teamcode.drive.MecanumDrive;
+
+@Autonomous
+public class SampleOpMode extends LinearOpMode {
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+
+        //MecanumDrive drive = new MecanumDrive(hardwareMap);
+
+        // Wait for the start signal
+        waitForStart();
+
+        // Example: Set the robot's pose
+        //drive.setPoseEstimate(beginPose);
+
+        // Add your autonomous logic here
+    }
+}
