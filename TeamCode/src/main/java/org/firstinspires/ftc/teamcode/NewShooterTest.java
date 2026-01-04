2865a492b5fecece9407bde016090512bf7fb481
Minh Pham <mnp.usvn@gmail.com>
2026-01-04 12:56:30 -0800

    main code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewShooterTest.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
@@ -22,6 +21,8 @@ public class NewShooterTest extends LinearOpMode{
 
     public void runOpMode() throws InterruptedException {
         // Map motors and servos
+
+        // Shooter
         DcMotorEx rightShooter = hardwareMap.get(DcMotorEx.class, "rightShooter");
         rightShooter.setDirection(DcMotorSimple.Direction.FORWARD);
         rightShooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -33,16 +34,40 @@ public class NewShooterTest extends LinearOpMode{
         DcMotorEx intakeMotor = hardwareMap.get(DcMotorEx.class, "intakeMotor");
         intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Turret
+        DcMotorEx turretMotor = hardwareMap.get(DcMotorEx.class, "turretMotor");
+        turretMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        turretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         Servo rightLatch = hardwareMap.get(Servo.class, "rightLatch");
         Servo leftLatch = hardwareMap.get(Servo.class, "leftLatch");
         Servo hood = hardwareMap.get(Servo.class, "hoodServo");
+
+        // Drivetrain
+        DcMotorEx frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        DcMotorEx backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        DcMotorEx backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Light
         Servo light = hardwareMap.get(Servo.class, "light");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
+        // Presets
+        double COUNTS_PER_MOTOR_REV = 28.0;
+
 
         // variables
 
@@ -52,7 +77,9 @@ public class NewShooterTest extends LinearOpMode{
 
         int latch = 0; // 0 = closed, 1 = open
 
-        ElapsedTime cooldown = new ElapsedTime();
+        double drivetrainMaxPower = 0.8;
+
+        double turretPower;
 
         double hoodAngle = 1; // 1 is all the way down, 0 is all the way up
 
@@ -60,17 +87,21 @@ public class NewShooterTest extends LinearOpMode{
         leftLatch.setPosition(latch); // 0 is closed, 1 is open
         hood.setPosition(hoodAngle);
 
+        ElapsedTime cooldown = new ElapsedTime();
+
         waitForStart();
         resetRuntime();
 
         while (opModeIsActive()) {
+
+            turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
             /// Control Settings
 
             // Shooter
             if (gamepad2.right_trigger > 0) {
                 /// IMPORTANT: leftShooter must be set at negative, to reverse direction
-                shooterSpeed = 1400;
+                shooterSpeed = 2000;
             } else if (gamepad2.left_trigger > 0) {
                 shooterSpeed = -300;
             } else {
@@ -78,7 +109,7 @@ public class NewShooterTest extends LinearOpMode{
             }
 
             // Latch
-            if (gamepad2.right_bumper && (cooldown.milliseconds() > 200)) {
+            if (gamepad2.right_bumper && (cooldown.milliseconds() > 300)) {
                 latch = 1 - latch;
                 cooldown.reset();
             }
@@ -87,11 +118,11 @@ public class NewShooterTest extends LinearOpMode{
 
             if (gamepad2.dpad_up) {
                 if (hoodAngle > 0) {
-                    hoodAngle -= 0.01;
+                    hoodAngle -= 0.05;
                 }
             } else if (gamepad2.dpad_down) {
                 if (hoodAngle < 1) {
-                    hoodAngle += 0.01;
+                    hoodAngle += 0.05;
                 }
             }
 
@@ -103,10 +134,24 @@ public class NewShooterTest extends LinearOpMode{
                 intakeMotor.setPower(0);
             }
 
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator * drivetrainMaxPower;
+            double backLeftPower = (y - x + rx) / denominator * drivetrainMaxPower;
+            double frontRightPower = (y - x - rx) / denominator * drivetrainMaxPower;
+            double backRightPower = (y + x - rx) / denominator * drivetrainMaxPower;
+
+            turretPower = gamepad2.right_stick_x * 0.5; // 0.5 for testing speed
+
             ///  Set Power to Motors/Servos
 
             rightShooter.setVelocity(shooterSpeed);
             leftShooter.setVelocity(shooterSpeed);
+
+            turretMotor.setPower(turretPower);
 
             rightLatch.setPosition(1 - latch);
             leftLatch.setPosition(latch);
@@ -115,11 +160,16 @@ public class NewShooterTest extends LinearOpMode{
 
             double avgShooterVel = (leftShooter.getVelocity() + rightShooter.getVelocity())/2;
 
-            if (avgShooterVel > 1380 && avgShooterVel < 1420) {
+            if (avgShooterVel > 1950 && avgShooterVel < 2050) {
                 light.setPosition(0.5);
             } else {
-                light.setPosition(0.3);
+                light.setPosition(0.28);
             }
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
 
             telemetry.addData("Shooter Velocity", avgShooterVel);
             telemetry.addData("Shooter Angle", hoodAngle);
@@ -128,8 +178,5 @@ public class NewShooterTest extends LinearOpMode{
 
         }
 
-
-
-
-        }
+    }
 }
