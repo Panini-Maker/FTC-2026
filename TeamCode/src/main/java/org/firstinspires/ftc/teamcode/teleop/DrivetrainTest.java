011d04033f69655c22f30e7aec7fba6d8e85926d
Minh Pham <mnp.usvn@gmail.com>
2026-02-01 13:53:55 -0800

    Autoaim + Shooter Speed + hood calculation 2/1/26

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DrivetrainTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/DrivetrainTest.java
@@ -1,12 +1,11 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 //import com.qualcomm.robotcore.hardware.Servo;
 
 //@TeleOp
-public class Test extends LinearOpMode {
+public class DrivetrainTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
